c3518be14f88012fa2cdd83a279902a1f9f6c403
hsneider Salgado Yate <hsneider_salgado14@hotmail.com>
2019-10-14 01:48:28 -0500

    mapa agregado

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseStorageService } from '../firebase-storage.service';
+import { AngularFireStorageReference } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseStorage: FirebaseStorageService) { }
 
-  ngOnInit() {
+  public referencia: AngularFireStorageReference;
+  public URLMainMap: any;
+  public URLMainMapHover: any;
+
+  public color="red";
+  ngOnInit(){
+    console.log('ngOnit');
+    this.referencia = this.firebaseStorage.referenciaCloudStorage('Ubicación-Usiacurí_Atlántico.png');
+    this.referencia.getDownloadURL().subscribe((URL1) => {
+      this.URLMainMap = URL1;
+      console.log(this.URLMainMap);
+    });
+    this.referencia = this.firebaseStorage.referenciaCloudStorage('Diseño-atlantico-mapa-minimalista.png');
+    this.referencia.getDownloadURL().subscribe((URL2) => {
+      this.URLMainMapHover = URL2;
+      console.log(this.URLMainMapHover);
+    });
   }
+  
 
 }
