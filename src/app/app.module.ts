9fbcf17859dcb7b801bde4676265c1d25839b792
hsalgadoy <52671523+hsalgadoy@users.noreply.github.com>
2019-11-04 19:02:52 -0500

    casi terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CaminomodaComponent } from './caminomoda/caminomoda.component';
 import { BoomComponent } from './boom/boom.component';
 import { LlegadacolorComponent } from './llegadacolor/llegadacolor.component';
 import { CadenaComponent } from './cadena/cadena.component';
+import { TestimoniosComponent } from './testimonios/testimonios.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CadenaComponent } from './cadena/cadena.component';
     CaminomodaComponent,
     BoomComponent,
     LlegadacolorComponent,
-    CadenaComponent
+    CadenaComponent,
+    TestimoniosComponent
   ],
   imports: [
     BrowserModule,
